6f807aecdacd5260f9bf94901c00ddc033f0bd65
Huntertk <factsofuniverse8@gmail.com>
2023-01-12 20:20:36 +0530

    Food Odering App Version 1

---
+++ index.js
@@ -0,0 +1,135 @@
+const menuArray = [
+    {
+        id: 0,
+        quantity:1,
+        name: "Chicken Tandoori",
+        ingredients: ["Smokey", "Grilled", "Tandoori"],
+        price: 800,
+        description:"Smokey Grilled Chicken Tandoori With Indian Spices.",
+        img: "./images/chicken.jpg",
+        orderBooked : false
+    },
+    {
+        id: 1,
+        quantity:1,
+        name: "Chicken Biryani",
+        ingredients: ["Dum Biryani", "Special Mint Raita", "Juicy Chicken"],
+        price: 450,
+        description:"Dum cooked fresh Handi Biryani. Using highest quality ingredients & authentic recipes.",
+        img: "./images/chicken-biryani.jpg",
+        orderBooked : false
+    },
+    {
+        id: 2,
+        quantity:1,
+        name: "Masala Dosa",
+        ingredients: ["Dosa", "Special Mint Raita", "Sambar"],
+        price: 200,
+        description:"Dosas are served hot, often with chutney and sambar.",
+        img: "./images/dosa.jpg",
+        orderBooked : false
+    }
+]
+const formDetails = document.getElementById('payment-form')
+const orderSummaryArr = []
+let priceTotal
+
+
+formDetails.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const formDetailsData = new FormData(formDetails)
+    const fullName = formDetailsData.get('fullName')
+    console.log(fullName)
+
+    document.getElementById('modal').style.display = 'none'
+    setTimeout(function(){
+        document.getElementById('customer-name').textContent = fullName
+        document.getElementById('final-message modal').style.display = 'block'
+        
+    },1000)
+})
+
+document.addEventListener('click',function(event){
+    if(event.target.dataset.increment){
+        handleAddingOrder(event.target.dataset.increment)   
+    } else if(event.target.dataset.decrement){
+        handleRemovingOrders(event.target.dataset.decrement)
+    } else if(event.target.id){
+        handleBookingFood(event.target.id)
+    }
+})
+
+function handleBookingFood(btnId){
+    if(btnId === 'complete-order' && orderSummaryArr.length>0){
+        document.getElementById('modal').style.display = 'block'
+    }
+}
+
+function handleRemovingOrders(orderRemoveId){
+    const targetOrderObj = menuArray.filter(function(menus){
+        return menus.id === Number(orderRemoveId)
+    })[0]
+    console.log(orderSummaryArr)
+        orderSummaryArr.pop()
+        renderOrder() 
+}
+
+function handleAddingOrder(oderAddId){
+    const targetOrderObj = menuArray.filter(function(menus){
+        return menus.id === Number(oderAddId)
+    })[0]
+        
+    
+            orderSummaryArr.push({
+                name: targetOrderObj.name,
+                price: targetOrderObj.price,
+                quantity: targetOrderObj.quantity
+            })
+            renderOrder()
+        
+    console.log(orderSummaryArr)
+}
+
+function renderOrder(){   
+    let order = ``
+
+    orderSummaryArr.forEach(function(orders){
+         order += `
+         <div class="order-item">
+            <p>${orders.name}</p>
+            <p id="quantity">X${orders.quantity}</p>
+            <button class="remove-btn" id="remove-btn">Remove</button>
+            <p id="total">₹${orders.price}</p>   
+        </div>    
+    `
+})
+document.getElementById('order-item').innerHTML = order  
+}
+
+function getMenu(){
+ let ingred = ``   
+let menuHtml = ``
+    menuArray.forEach(function(menu){
+            ingred = menu.ingredients
+            console.log(ingred)
+            menuHtml += `
+            <div id="container">
+            <img class="food-img" src="${menu.img}" alt="">
+            <div class="food-container">
+            <p class="dish-name">${menu.name}</p>
+            <p class="ingrediants">${ingred[0]}, ${ingred[1]}, ${ingred[2]}</p>
+            <span class="discount price">₹${menu.price}</span>
+            <p class="Description">${menu.description}</p>
+            </div>
+            <div class="buy">
+            <button class="btnBuy" data-decrement=${menu.id}>-</button>
+            <button class="btnBuy" data-increment=${menu.id}>+</button>
+            </div>
+            </div>
+            `
+        })
+    document.getElementById('container-render').innerHTML = menuHtml
+}
+
+getMenu()
